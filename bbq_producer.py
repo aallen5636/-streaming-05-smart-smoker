6f260cb9cc36c0bfa221f969338a8b99e602de5f
aallen5636 <aallen5636@gmail.com>
2023-02-08 17:10:11 -0600

    Running Code

--- bbq_producer.py
+++ bbq_producer.py
@@ -8,163 +8,95 @@ This program reads in a csv file and for each row sends
 """
 
 # import python modules
+
 import pika
 import sys
 import webbrowser
 import csv
-# import socket
 import time
-
-
-# define variables/constants/options
-host = "localhost"
-# port = 9999
-# address_tuple = (host, port)
-csv_file = "smoker-temps.csv"
-smoker_queue = "01-smoker"
-foodA_queue = "02-food-A"
-foodB_queue = "03-food-B"
-show_offer = True # (RabbitMQ Server option - T=on, F=off)
-# socket_family = socket.AF_INET 
-# socket_type = socket.SOCK_DGRAM
-
-
-# define functions
-## define option to open RabbitMQ admin webpage
+#Show Rabbit MQ admin page if show_offer is True
 def offer_rabbitmq_admin_site(show_offer):
-    # includes show_offer variable - option to turn off the offer later in the code
-    if show_offer == True:
-        """Offer to open the RabbitMQ Admin website"""
+    """Offer to open the RabbitMQ Admin website"""
+    if show_offer==True:
         ans = input("Would you like to monitor RabbitMQ queues? y or n ")
         print()
         if ans.lower() == "y":
             webbrowser.open_new("http://localhost:15672/#/queues")
             print()
 
-## define delete_queue
-def delete_queue(host: str, queue_name: str):
-    """
-    Delete queues each time we run the program to clear out old messages.
-    """
-    conn = pika.BlockingConnection(pika.ConnectionParameters(host))
-    ch = conn.channel()
-    ch.queue_delete(queue=queue_name)
-
-## define main work of program
-
-### define a message to send to queue
-def publish_message_to_queue(host: str, queue_name: str, message: str):
+def send_message(host: str, queue_name: str, message: str):
     """
     Creates and sends a message to the queue each execution.
     This process runs and finishes.
-
     Parameters:
         host (str): the host name or IP address of the RabbitMQ server
         queue_name (str): the name of the queue
         message (str): the message to be sent to the queue
     """
-    ### Get a connection to RabbitMQ and create a channel
+
     try:
-        # create a connection to the RabbitMQ server
+        # create a blocking connection to the RabbitMQ server
         conn = pika.BlockingConnection(pika.ConnectionParameters(host))
         # use the connection to create a communication channel
         ch = conn.channel()
-        # declare a durable queue (will survive a RabbitMQ server restart
-        # and help ensure messages are processed in order)
+        # use the channel to declare a durable queue
+        # a durable queue will survive a RabbitMQ server restart
+        # and help ensure messages are processed in order
         # messages will not be deleted until the consumer acknowledges
         ch.queue_declare(queue=queue_name, durable=True)
-        # use the channel to publish a message to the queue; each message passes through an exchange
+        # use the channel to publish a message to the queue
+        # every message passes through an exchange
         ch.basic_publish(exchange="", routing_key=queue_name, body=message)
         # print a message to the console for the user
-        print(f" [x] Sent {message} to {queue_name}")
+        print(f" [x] Sent {message}")
     except pika.exceptions.AMQPConnectionError as e:
         print(f"Error: Connection to RabbitMQ server failed: {e}")
         sys.exit(1)
     finally:
         # close the connection to the server
         conn.close()
+#Declare a host name
+host="localhost"
+#Set Queueclear to true to clear the Queue
+Queueclear=True
+def offer_Queueclear(Queueclear):
+    """Offer to open the RabbitMQ Admin website"""
+    if Queueclear==True:
+        ans = input("Would you like to clear the queues? y or n ")
+        print()
+        if ans.lower() == "y":
+            # create a blocking connection to the RabbitMQ server
+            conn = pika.BlockingConnection(pika.ConnectionParameters(host))
+            # use the connection to create a communication channel
+            ch = conn.channel()
+            #delete the queues
+            ch.queue_delete("01-smoker")
+            ch.queue_delete("02-food-A")
+            ch.queue_delete("03-food-B")
+            print("Queue cleared!")
 
-def get_message_from_csv(input_file):
-    """
-    Read from csv input file. Send each row as a message to the queue.
-    """ 
-    # sock = socket.socket(socket_family, socket_type) 
-
-    # read from a csv file
-    input_file = open(csv_file, "r")
-    reader = csv.reader(input_file, delimiter=',')
-
-    # Skip reading the header row of csv
-    next(reader)
-
-    # Write the header row to the output file
-    # header_list = ['Time (UTC)', 'Channel1', 'Channel2', 'Channel3']
-
+offer_Queueclear(Queueclear)
+""" Create csv function to read from file and turn it into a message"""
+#opens smoker File
+with open("smoker-temps.csv",'r') as file:
+    reader=csv.reader(file,delimiter=",")
+#read in rows 
     for row in reader:
-        input_string_row1 = row[1]
-        input_string_row2 = row[2]
-        input_string_row3 = row[3]
-
-        to_convert_column1 = input_string_row1.replace('', '0')
-        to_convert_column2 = input_string_row2.replace('', '0')
-        to_convert_column3 = input_string_row3.replace('', '0')
-
-        float_row1 = float(to_convert_column1)
-        float_row2 = float(to_convert_column2)
-        float_row3 = float(to_convert_column3)
-
-        # get the "timestamp" column 
-        fstring_time = f"{row[0]}"
-        fstring_channel1 = f"{row[1]}"
-        fstring_channel2 = f"{row[2]}"
-        fstring_channel3 = f"{row[3]}"
-
-        # convert columns to float types
-        # Channel1 = float(row[1])
-        # Channel2 = float(row[2])
-        # Channel3 = float(row[3])
-
-        # use an fstring to create messages from our data
-        fstring_message_smoker = f"[{fstring_time}, {fstring_channel1}]"
-        fstring_message_foodA = f"[{fstring_time}, {fstring_channel2}]"
-        fstring_message_foodB = f"[{fstring_time}, {fstring_channel3}]"
-
-        # used this code in mod4, but not mod2... not sure if needed
-        # input_file.read
-
-        # prepare a binary (1s and 0s) message to stream
-        # 'message' is case sensitive!
-        message_smoker = fstring_message_smoker.encode()
-        message_foodA = fstring_message_foodA.encode()
-        message_foodB = fstring_message_foodB.encode()
-
-        # use the socket sendto() method to send the message
-        # sock.sendto(message_smoker, address_tuple)
-        # sock.sendto(message_foodA, address_tuple)
-        # sock.sendto(message_foodB, address_tuple)
-
-        # publish message to queue
-        # publish_message_to_queue(host, smoker_queue, message_smoker)
-        # publish_message_to_queue(host, foodA_queue, message_foodA)
-        # publish_message_to_queue(host, foodB_queue, message_foodB)
-
-        # publish using routing
-        if float_row1 > 0: publish_message_to_queue(host, smoker_queue, message_smoker)
-        if float_row2 > 0: publish_message_to_queue(host, foodA_queue, message_foodA)
-        if float_row3 > 0: publish_message_to_queue(host, foodB_queue, message_foodB)
-        else: print()
-
-        # slowly read a row every 1 seconds from file
-        time.sleep(1)        
-
-
-# Run program
-if __name__ == "__main__":  
-    # if show_offer = True, ask the user if they'd like to open the RabbitMQ Admin site
-    offer_rabbitmq_admin_site(show_offer)
-    # delete queues to clear old messages
-    delete_queue(host, smoker_queue)
-    delete_queue(host, foodA_queue)
-    delete_queue(host, foodB_queue)
-    # get the message from the csv input file and send to queue
-    get_message_from_csv(csv_file)
+     #Read just the timestamp and store it
+        fstringtime=f"{row[0]}"
+        #Read smoker reading and store it
+        smokertemp=f"{row[1]}"
+        #Read Food A reading and store it
+        foodA=f"{row[2]}"
+        #Read Food B reading and store it
+        foodB=f"{row[3]}"
+        #Set up messages 
+        smoker_message=f"{fstringtime},{smokertemp}"
+        foodA_message=f"{fstringtime},{foodA}"
+        foodB_message=f"{fstringtime},{foodB}"
+        #Get those messages rolling out!
+        send_message(host,"01-smoker",smoker_message)
+        send_message(host,"02-food-A",foodA_message)
+        send_message(host,"03-food-B",foodB_message)
+        #Slow down the output to one message every 30 seconds
+        time.sleep(30)
